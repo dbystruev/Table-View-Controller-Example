6a81b42d73d53caeed4874086e91c6bb9f7f169f
Denis Bystruev <dbystruev@me.com>
2019-03-04 22:02:39 +0300

    Initial Commit

---
+++ Table View Controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Table View Controller
+//
+//  Created by Denis Bystruev on 04/03/2019.
+//  Copyright © 2019 Denis Bystruev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
